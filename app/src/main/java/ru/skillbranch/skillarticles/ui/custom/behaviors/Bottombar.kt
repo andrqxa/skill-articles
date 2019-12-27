79d123dda9bd1c1b8a0300f26463f1f13d2b0e40
Andrii Puhachenko <andrqxa@gmail.com>
2019-12-28 01:09:09 +0200

    Merge instead of FrameLayout

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/Bottombar.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/Bottombar.kt
@@ -3,14 +3,14 @@ package ru.skillbranch.skillarticles.ui.custom
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import ru.skillbranch.skillarticles.R
 
 class Bottombar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
     init {
 //        View.inflate(context, R.layout.layout_bottombar, this)
 //        val materialBg = MaterialShapeDrawable.createWithElevationOverlay(context)
