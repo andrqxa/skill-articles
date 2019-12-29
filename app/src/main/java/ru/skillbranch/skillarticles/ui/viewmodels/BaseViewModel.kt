f18afdd502e3824f332048090b57785cb7f6940f
Andrii Puhachenko <andrqxa@gmail.com>
2019-12-30 01:56:35 +0200

    Added BaseViewModel 01h00m24sec

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/viewmodels/BaseViewModel.kt
@@ -0,0 +1,42 @@
+package ru.skillbranch.skillarticles.ui.viewmodels
+
+import androidx.annotation.UiThread
+import androidx.lifecycle.*
+
+abstract class BaseViewModel<T>(initState: T): ViewModel() {
+    protected val state: MediatorLiveData<T> = MediatorLiveData<T>().apply {
+        value = initState
+    }
+
+//    not null current state
+    protected val currentState
+        get() = state.value!!
+
+    /***
+    * лямбда выражение принимает в качестве аргумента лямбду в которую передается текущее состояние
+    * и она возвращает модифицированное состояние которое присваивается текущему состоянию
+    */
+    @UiThread
+    protected inline fun updateState(update: (currentState: T) -> T){
+        val updatedState: T = update(currentState)
+        state.value = updatedState
+    }
+
+    /***
+    * более компактная форма записи observe принимает последним аргументом лямбду обрабатывающую
+    * изменение текущего состояния
+    */
+    fun observeState(owner: LifecycleOwner, onChanged: (newState: T) -> Unit){
+        state.observe(owner, Observer { onChanged(it!!) })
+    }
+}
+
+class ViewModelFactory(private val params: String): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ArticleViewModel::class.java)){
+            return ArticleViewModel(params) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class ")
+    }
+
+}
