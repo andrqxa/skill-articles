0c9ae5efbca57d80edcb288aa6c4f0b9464f6b68
Andrii Puhachenko <andrqxa@gmail.com>
2020-01-01 21:48:09 +0200

    Added ArticleViewModel class 01h00m56sec

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/viewmodels/ArticleViewModel.kt
@@ -0,0 +1,27 @@
+package ru.skillbranch.skillarticles.ui.viewmodels
+
+class ArticleViewModel(articleId: String): BaseViewModel<ArticleState>(ArticleState())
+
+data class ArticleState (
+    val isAuth: Boolean = false, //пользовватель авторизован
+    val isLoadingContent: Boolean = true, // контент загружается
+    val isLoadingReviews: Boolean = true, // отзывы загружаются
+    val isLike: Boolean = false, // отмечено как Like
+    val isBookmark: Boolean = false, // в закладках
+    val isShowMenu: Boolean = false, // отображается меню
+    val isBigText: Boolean = false, // шрифт увеличен
+    val isDarkMode: Boolean = false, // темный режим
+    val isSearch: Boolean = false, // режим поиска
+    val searchQuery: String? = null, // поисковый запрос
+    val searchResult: List<Pair<Int, Int>> = emptyList(), // результаты поиска (стартовая и конечная позиция)
+    val searchPosition: Int = 0, // текущая позиция найденного результата
+    val shareLink: String? = null, // ссылка Share
+    val title: String? = null, // заголовок статьи
+    val category: String? = null, // категория
+    val categoryIcon: Any? = null, // иконка категории
+    val date: String? = null, // дата публикации
+    val author: Any? = null, // автор статьи
+    val poster: String? = null, // обложка статьи
+    val content: List<Any> = emptyList(), // контент
+    val reviews: List<Any> = emptyList() // комментарии
+)
