6deded59c580e18fcfceb8e8d54f7a5010717105
Andrii Puhachenko <andrqxa@gmail.com>
2020-01-09 23:51:31 +0200

    Added toolbar behavior

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/ToolbarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/ToolbarBehavior.kt
@@ -5,33 +5,21 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
-import kotlin.math.max
 import kotlin.math.min
 
-class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
+class ToolbarBehavior<V : View>(context: Context, attrs: AttributeSet) :
     CoordinatorLayout.Behavior<V>(context, attrs) {
 
     override fun onStartNestedScroll(
-        coordinatorLayout: CoordinatorLayout,
-        child: V,
-        directTargetChild: View,
-        target: View,
-        axes: Int,
-        type: Int
+        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
     ): Boolean {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
     override fun onNestedPreScroll(
-        coordinatorLayout: CoordinatorLayout,
-        child: V,
-        target: View,
-        dx: Int,
-        dy: Int,
-        consumed: IntArray,
-        type: Int
+        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
     ) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+        child.translationY = min(0f, min(child.height.toFloat(), child.translationY - dy))
     }
 }
