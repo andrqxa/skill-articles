a8f5a3e68a7e4ce8a93147640b76c7f59acc1845
Andrii Puhachenko <andrqxa@gmail.com>
2020-02-04 23:40:55 +0200

    Added from video behavior with my modifications

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/ToolbarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/ToolbarBehavior.kt
@@ -5,21 +5,34 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import com.google.android.material.appbar.MaterialToolbar
 import kotlin.math.min
 
-class ToolbarBehavior<V : View>(context: Context, attrs: AttributeSet) :
-    CoordinatorLayout.Behavior<V>(context, attrs) {
+class ToolbarBehavior() : CoordinatorLayout.Behavior<MaterialToolbar>() {
+    constructor(context: Context, attrs: AttributeSet) : this()
 
     override fun onStartNestedScroll(
-        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
+        coordinatorLayout: CoordinatorLayout,
+        child: MaterialToolbar,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
     ): Boolean {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
     override fun onNestedPreScroll(
-        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+        coordinatorLayout: CoordinatorLayout,
+        child: MaterialToolbar,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
     ) {
+        val offset = min(0f, min(child.height.toFloat(), child.translationY - dy))
+        if (offset != child.translationY) child.translationY = offset
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = min(0f, min(child.height.toFloat(), child.translationY - dy))
     }
 }
