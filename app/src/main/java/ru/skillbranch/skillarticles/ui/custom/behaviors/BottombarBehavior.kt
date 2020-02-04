ed6378b03a6759e0801e9861e48dcb1dfc99316e
Andrii Puhachenko <andrqxa@gmail.com>
2020-02-05 01:23:49 +0200

    Deleted ToolbarBehavior

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -6,14 +6,16 @@ import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.math.MathUtils
 import androidx.core.view.ViewCompat
+import com.google.android.material.appbar.MaterialToolbar
 import ru.skillbranch.skillarticles.ui.custom.Bottombar
+import kotlin.math.min
 
-class BottombarBehavior() : CoordinatorLayout.Behavior<Bottombar>() {
+class BottombarBehavior<V : View>() : CoordinatorLayout.Behavior<V>() {
     constructor(context: Context, attrs: AttributeSet) : this()
 
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: Bottombar,
+        child: V,
         directTargetChild: View,
         target: View,
         axes: Int,
@@ -24,14 +26,18 @@ class BottombarBehavior() : CoordinatorLayout.Behavior<Bottombar>() {
 
     override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
-        child: Bottombar,
+        child: V,
         target: View,
         dx: Int,
         dy: Int,
         consumed: IntArray,
         type: Int
     ) {
-        val offset = MathUtils.clamp(child.translationY + dy, 0f, child.height.toFloat())
+        val offset = when (child) {
+            is Bottombar -> MathUtils.clamp(child.translationY + dy, 0f, child.height.toFloat())
+            is MaterialToolbar -> min(0f, min(child.height.toFloat(), child.translationY - dy))
+            else -> 0f
+        }
         if (offset != child.translationY) child.translationY = offset
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
