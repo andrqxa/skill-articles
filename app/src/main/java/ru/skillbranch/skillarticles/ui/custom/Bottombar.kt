157a064dd7489282cd408c92689d2f2e727ca036
Andrii Puhachenko <andrqxa@gmail.com>
2020-02-06 19:47:06 +0200

    L04: 36m23sec, Work with Bottombar

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -5,13 +5,17 @@ import android.os.Parcel
 import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
 import com.google.android.material.shape.MaterialShapeDrawable
 import kotlinx.android.synthetic.main.layout_bottombar.view.*
+import kotlinx.android.synthetic.main.layout_submenu.view.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.ui.custom.behaviors.BottombarBehavior
+import kotlin.math.hypot
 
 class Bottombar @JvmOverloads constructor(
     context: Context,
@@ -45,6 +49,58 @@ class Bottombar @JvmOverloads constructor(
             isSearchMode = state.ssIsSearchMode
             reveal.isVisible = !isSearchMode
             group_bottom.isVisible = !isSearchMode
+        }
+    }
+
+    fun setSearchState(search: Boolean) {
+        if (isSearchMode == search || !isAttachedToWindow) return
+        isSearchMode = search
+        if (isSearchMode) animateShowSearchPanel()
+        else animateHideSearchPanel()
+    }
+
+    private fun animateHideSearchPanel() {
+        group_bottom.isVisible = false
+        val endRadius = hypot(width.toFloat(), height / 2f)
+        val va = ViewAnimationUtils.createCircularReveal(
+            reveal,
+            width,
+            height / 2,
+            endRadius,
+            0f
+        )
+        va.doOnEnd { reveal.isVisible = false }
+        va.start()
+    }
+
+    private fun animateShowSearchPanel() {
+        reveal.isVisible = false
+        val endRadius = hypot(width.toFloat(), height / 2f)
+        val va = ViewAnimationUtils.createCircularReveal(
+            reveal,
+            width,
+            height / 2,
+            0f,
+            endRadius
+        )
+        va.doOnEnd { group_bottom.isVisible = false }
+        va.start()
+    }
+
+    fun bindSearchInfo(searchCount: Int = 0, position: Int = 0) {
+        if (searchCount == 0) {
+            tv_search_result.text = "Not found"
+            btn_text_up.isEnabled = false
+            btn_text_down.isEnabled = false
+        } else {
+            tv_search_result.text = "${position.inc()} of $searchCount"
+            btn_text_up.isEnabled = true
+            btn_text_down.isEnabled = true
+        }
+
+        when (position) {
+            0 -> btn_result_up.isEnabled = false
+            -1 -> btn_result_down.isEnabled = false
         }
     }
 
