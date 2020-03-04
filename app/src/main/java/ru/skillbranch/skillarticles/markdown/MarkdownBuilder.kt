de74c760300ddfddab3da8a29f1b0353df2fb2a7
Andrii Puhachenko <andrqxa@gmail.com>
2020-03-04 22:51:50 +0200

    L05: 01h39m40s - TESTS ARE BROKEN

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
@@ -12,18 +12,19 @@ import ru.skillbranch.skillarticles.markdown.spans.UnorderedListSpan
 
 class MarkdownBuilder(context: Context) {
     private val colorSecondary = context.attrValue(R.attr.colorSecondary)
-    private val colorPrimary = context.attrValue(R.attr.colorPrimary)
-    private val colorDivider = context.getColor(R.color.color_divider)
-    private val colorOnSurface = context.attrValue(R.attr.colorOnSurface)
-    private val colorSurface = context.attrValue(R.attr.colorSurface)
+
+    //    private val colorPrimary = context.attrValue(R.attr.colorPrimary)
+//    private val colorDivider = context.getColor(R.color.color_divider)
+//    private val colorOnSurface = context.attrValue(R.attr.colorOnSurface)
+//    private val colorSurface = context.attrValue(R.attr.colorSurface)
     private val gap: Float = context.dpToPx(8)
     private val bulletRadius = context.dpToPx(4)
-    private val strikeWidth = context.dpToPx(4)
-    private val headerMarginTop = context.dpToPx(12)
-    private val headerMarginBottom = context.dpToPx(8)
-    private val ruleWidth = context.dpToPx(2)
-    private val cornerRadius = context.dpToPx(8)
-    private val linkIcon = context.getDrawable(R.drawable.ic_link_black_24dp)!!
+//    private val strikeWidth = context.dpToPx(4)
+//    private val headerMarginTop = context.dpToPx(12)
+//    private val headerMarginBottom = context.dpToPx(8)
+//    private val ruleWidth = context.dpToPx(2)
+//    private val cornerRadius = context.dpToPx(8)
+//    private val linkIcon = context.getDrawable(R.drawable.ic_link_black_24dp)!!
 
     fun markdownToSpan(string: String): SpannedString {
         val markdown = MarkdownParser.parse(string)
@@ -43,6 +44,7 @@ class MarkdownBuilder(context: Context) {
                         }
                     }
                 }
+                else -> append(element.text)
             }
         }
     }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -25,6 +25,7 @@ import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.R.id.search_src_text
 import ru.skillbranch.skillarticles.extensions.dpToIntPx
 import ru.skillbranch.skillarticles.extensions.setMarginOptionally
+import ru.skillbranch.skillarticles.markdown.MarkdownBuilder
 import ru.skillbranch.skillarticles.ui.base.BaseActivity
 import ru.skillbranch.skillarticles.ui.base.Binding
 import ru.skillbranch.skillarticles.ui.custom.SearchFocusSpan
@@ -279,7 +280,12 @@ class RootActivity : BaseActivity<ArticleViewModel>(), IArticleView {
         private var searchPosition: Int by ObserveProp(0)
 
         private var content: String by ObserveProp("loading") {
-            tv_text_content.setText(it, TextView.BufferType.SPANNABLE)
+            MarkdownBuilder(this@RootActivity)
+                .markdownToSpan(it)
+                .run {
+                    tv_text_content.setText(this, TextView.BufferType.SPANNABLE)
+                }
+
             tv_text_content.movementMethod = ScrollingMovementMethod()
         }
 
@@ -316,7 +322,7 @@ class RootActivity : BaseActivity<ArticleViewModel>(), IArticleView {
             if (data.title != null) title = data.title
             if (data.category != null) category = data.category
             if (data.categoryIcon != null) categoryIcon = data.categoryIcon as Int
-            if (data.content != null) content = data.content.first() as String
+            if (data.content != null) content = data.content
 
             isLoadingContent = data.isLoadingContent
             isSearch = data.isSearch
