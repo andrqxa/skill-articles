65abe60cf009521db24e7510f4208a60f76083a5
Andrii Puhachenko <andrqxa@gmail.com>
2020-03-06 00:30:29 +0200

    L05: 01h41m48s - Fix tests and add drawLeadingMargin

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnorderedListSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnorderedListSpan.kt
@@ -26,7 +26,18 @@ class UnorderedListSpan(
         lineTop: Int, lineBaseline: Int, lineBottom: Int, text: CharSequence?, lineStart: Int,
         lineEnd: Int, isFirstLine: Boolean, layout: Layout?
     ) {
-        //TODO implement me
+        // only for first line draw bullet
+        if (isFirstLine) {
+            val oldColor = paint.color
+            paint.color = bulletColor
+            canvas.drawCircle(
+                gapWidth + currentMarginLocation + bulletRadius,
+                (lineTop + lineBottom) / 2f,
+                bulletRadius,
+                paint
+            )
+            paint.color = oldColor
+        }
     }
 
     private inline fun Paint.withCustomColor(block: () -> Unit) {
