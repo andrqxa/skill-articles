6ed42a0b807265a696716532142e8fdb78fc5e0e
Andrii Puhachenko <andrqxa@gmail.com>
2020-03-16 22:41:29 +0200

    L05: 01h57m30s - Added Header Span - DON'T WORK

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownBuilder.kt
@@ -11,23 +11,24 @@ import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.attrValue
 import ru.skillbranch.skillarticles.extensions.dpToPx
 import ru.skillbranch.skillarticles.markdown.spans.BlockquotesSpan
+import ru.skillbranch.skillarticles.markdown.spans.HeaderSpan
 import ru.skillbranch.skillarticles.markdown.spans.UnorderedListSpan
 
 class MarkdownBuilder(context: Context) {
+    private val colorSecondary = context.attrValue(R.attr.colorSecondary)
+    private val colorPrimary = context.attrValue(R.attr.colorPrimary)
+    private val colorDivider = context.getColor(R.color.color_divider)
+    private val colorOnSurface = context.attrValue(R.attr.colorOnSurface)
+    private val colorSurface = context.attrValue(R.attr.colorSurface)
     private val gap: Float = context.dpToPx(8)
     private val bulletRadius = context.dpToPx(4)
     private val quoteWidth = context.dpToPx(4)
-    private val colorSecondary = context.attrValue(R.attr.colorSecondary)
-    //    private val colorPrimary = context.attrValue(R.attr.colorPrimary)
-//    private val colorDivider = context.getColor(R.color.color_divider)
-//    private val colorOnSurface = context.attrValue(R.attr.colorOnSurface)
-//    private val colorSurface = context.attrValue(R.attr.colorSurface)
-//    private val strikeWidth = context.dpToPx(4)
-//    private val headerMarginTop = context.dpToPx(12)
-//    private val headerMarginBottom = context.dpToPx(8)
-//    private val ruleWidth = context.dpToPx(2)
-//    private val cornerRadius = context.dpToPx(8)
-//    private val linkIcon = context.getDrawable(R.drawable.ic_link_black_24dp)!!
+    private val strikeWidth = context.dpToPx(4)
+    private val headerMarginTop = context.dpToPx(12)
+    private val headerMarginBottom = context.dpToPx(8)
+    private val ruleWidth = context.dpToPx(2)
+    private val cornerRadius = context.dpToPx(8)
+    private val linkIcon = context.getDrawable(R.drawable.ic_link_black_24dp)!!
 
     fun markdownToSpan(string: String): SpannedString {
         val markdown = MarkdownParser.parse(string)
@@ -55,6 +56,19 @@ class MarkdownBuilder(context: Context) {
                         for (child in element.elements) {
                             buildElement(child, builder)
                         }
+                    }
+                }
+                is Element.Header -> {
+                    inSpans(
+                        HeaderSpan(
+                            element.level,
+                            colorPrimary,
+                            colorDivider,
+                            headerMarginTop,
+                            headerMarginBottom
+                        )
+                    ) {
+                        append(element.text)
                     }
                 }
                 else -> append(element.text)
