b978a7476fb31b185a94dfc33a96f6ea1139837a
Andrii Puhachenko <andrqxa@gmail.com>
2020-04-06 00:08:39 +0300

    L06: 01h53m12s - Entered MarkdownCodeView

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownBuilder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownBuilder.kt
@@ -12,7 +12,6 @@ import androidx.core.text.inSpans
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.data.repositories.Element
 import ru.skillbranch.skillarticles.data.repositories.MarkdownElement
-import ru.skillbranch.skillarticles.data.repositories.MarkdownParser
 import ru.skillbranch.skillarticles.extensions.attrValue
 import ru.skillbranch.skillarticles.extensions.dpToPx
 import ru.skillbranch.skillarticles.ui.custom.spans.*
@@ -37,9 +36,8 @@ class MarkdownBuilder(context: Context) {
     }
 
     fun markdownToSpan(textContent: MarkdownElement.Text): SpannedString {
-        val markdown = MarkdownParser.parse(string)
         return buildSpannedString {
-            markdown.elements.forEach { buildElement(it, this) }
+            textContent.elements.forEach { buildElement(it, this) }
         }
     }
 
@@ -119,19 +117,6 @@ class MarkdownBuilder(context: Context) {
                     inSpans(
                         IconLinkSpan(linkIcon, gap, colorPrimary, strikeWidth),
                         URLSpan(element.link)
-                    ) {
-                        append(element.text)
-                    }
-                }
-                is Element.BlockCode -> {
-                    inSpans(
-                        BlockCodeSpan(
-                            colorPrimary,
-                            colorSurface,
-                            cornerRadius,
-                            gap,
-                            element.type
-                        )
                     ) {
                         append(element.text)
                     }
